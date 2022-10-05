83bd724fdd983a26e4b798df3d42abd963e7f0c3
Oplp25 <jakobwood25@gmail.com>
2022-10-05 12:35:00 +0100

    Added main file

--- startWindow.py
+++ startWindow.py
@@ -4,5 +4,8 @@ def runStartWindow():
     layout=[[]]
     win=ps.Window(title='Start Configuration',layout=layout,margins=[250,250])
     win.read()
+    run=True
+    while run:
+        events,values=win.read()
 
 runStartWindow()
